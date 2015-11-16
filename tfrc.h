9cabfffd056196509e4fec1c37c5d9ff2268b44a
Fangyu <fangyuh@clemson.edu>
2015-11-15 19:23:25 -0500

    fix tfrc.h and do the send ok

--- tfrc.h
+++ tfrc.h
@@ -14,13 +14,17 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <sys/types.h>
+#include <string.h>     /* for memset() */
+#include <netinet/in.h> /* for in_addr */
+#include <sys/socket.h> /* for socket(), connect(), sendto(), and recvfrom() */
+#include <arpa/inet.h>  /* for sockaddr_in and inet_addr() */
+#include <stdlib.h>     /* for atoi() and exit() */
+#include <unistd.h>     /* for close() */
+
 
 /*largest amount of data on a socket read*/
 #define MAX_BUFFER 65000
 
-#ifndef LINUX
-#define INADDR_NONE 0xffffffff
-#endif
 
 /* Message type */
 #define CONTROL (0x01)
@@ -39,8 +43,8 @@ struct control_t {
     uint16_t code;      /* Code */
     uint32_t CxID;      
     uint32_t seqNum;    /* Sequence Number*/
-    uint32_t msgSize    /* Send Msg Size */
-}   
+    uint32_t msgSize;   /* Send Msg Size */
+};   
 
 /* Considered as Data Message Format */
 struct data_t {
@@ -52,7 +56,7 @@ struct data_t {
     uint32_t timeStamp; /* TimeStamp */
     uint32_t RTT;       /* Sender's RTT Estimate (in microsecond) */
     uint8_t  X[0];      /* Holds Msg Data */
-}
+};
 
 /* Considered as ACK Message Format */
 struct ACK_t {
@@ -65,6 +69,6 @@ struct ACK_t {
     uint32_t T_delay;   /* T_delay (in microsecond) */
     uint32_t lossRate;  /* Loss Event Rate */
     uint32_t recvRate;  /* Receive Rate (in bits per second) */
-}
+};
 
 void DieWithError(char *errorMessage);  /* External error handling function */
