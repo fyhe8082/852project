3ead7a77bd7f734bdb8d97602dd5504a01819f2f
Fangyu <fangyuh@clemson.edu>
2015-11-21 13:50:49 -0500

    delete the repeated include file

--- tfrc.h
+++ tfrc.h
@@ -11,7 +11,6 @@
 *
 *********************************************************/
 #include <stdio.h>
-#include <stdlib.h>
 #include <errno.h>
 #include <sys/types.h>
 #include <string.h>     /* for memset() */
