94e499aca603a319f24a54b60bd572b76c783bab
zhugexubin <zhugexubin@163.com>
2015-11-21 21:15:22 -0500

    client zhuge

--- client.c
+++ client.c
@@ -8,11 +8,46 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <sys/type.h>
+#include <sys/types.h>
 #include "tfrc_client.h"
 
-int main(int argc, char *argv[]) {
+
+void setupTcpConnection() {
+	memset(&(tfrc_client.servAddr), 0, sizeof(tfrc_client.servAddr)); 
+	tfrc_client.servAddr.sin_family = AF_INET;
+	tfrc_client.servAddr.sin_addr.s_addr = inet_addr(tfrc_client.servIP);
+
+	// resolve dotted decimal address
+	if(tfrc_client.servAddr.sin_addr.s_addr == -1) {
+		tfrc_client.thehost = gethostbyname(tfrc_client.servIP);
+		tfrc_client.servAddr.sin_addr.s_addr = *((unsigned long *) tfrc_client.thehost->h_addr_list[0]);
+	}
 	
+	tfrc_client.servAddr.sin_port = htons(tfrc_client.servPort);
+
+	if(tfrc_client.sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP) < 0)
+		DieWithError("socket() failed");
+}
+
+void setupCntrlMsg(uint16_t msgSize) {
+	cntrl.msgLength = htons(CNTRLMSGSIZE);
+	cntrl.msgType = CONTROL;
+	cntrl.code = START;
+	cntrl.CxID = htonl(OK);
+	cntrl.seqNum = htonl(rand()%MAXSEQ);  // limit the max to avoid overflow
+	cntrl.msgSize = htons(msgSize);
+}
+void setupDataMsg() {
+
+}
+
+void setupAckMsg() {
+
+}
+int main(int argc, char *argv[]) {
+
+
+    printf("client start!\n");
 	if(argc != 7) {
 		fprintf(stderr, "Usage: [<tfrc-client> <destinationAddress> <destinationPort> <messageSize> <connectionID> <simulatedLossRate> <maxAllowedThroughput>]\n");
 		exit(1);
@@ -25,11 +60,23 @@ int main(int argc, char *argv[]) {
 	tfrc_client.simulatedLossRate = atoi(argv[5]);
 	tfrc_client.maxAllowedThroughput = atoi(argv[6]);
 
+	//Sender Initialize Parameters
+	initializeparams();
+	setupTcpConnection();
+	setupCntrlMsg(tfrc_client.msgSize);
+	setupDataMsg();
+	setupAckMsg();
+
 	while(true) {
 		switch(clientStatus) {
 		case CLIENT_START:
-			
-
+			if(tfrc_client.alarmtimeout) {
+				alarm(0);
+				if(sendto(tfrc_client.sock, (char *)cntrl, CNTRLMSGSIZE, 0, (struct sockaddr *)
+						&(tfrc_client.servAddr), sizeof(tfrc_client.servAddrLen) != CNTRLMSGSIZE))
+					DieWithError("sendto() send a different number of bytes than expected");
+				tfrc_client.alarmtimeout = false;
+				alarm(tfrc_client.cntrlTimeout); // start the timeout
 		case CLIENT_SENDING:
 
 
--- tfrc_client.h
+++ tfrc_client.h
@@ -6,6 +6,14 @@
  ************************************************************************/
 #include "tfrc.h"
 
+#define CNTRLMSGSIZE 14
+#define ACKMSGSIZE 36
+#define DATAHEADERSIZE 24
+#define MAXSEQ 10000
+#define DATAMAX 1500
+#define MAXINITTRY 10
+#define true 1
+#define false 0
 typedef enum {
 	CLIENT_START,
 	CLIENT_SENDING,
@@ -24,11 +32,30 @@ struct ClientPrms {
 	char *servIP;            /* ip address of server */
 	struct hostent *thehost; /* hostent from gethostbyname() */
 	uint32_t connectionID;
-	uint32_t msgSize;        /* message size in bytes */
 
+	/********TRFC Params**************/
+	uint32_t msgSize;        /* message size in bytes */
 	float simulatedLossRate;
 	double maxAllowedThroughput;
+	double X_trans;  // allowed transmit rate in bytes/s
+	double X_bps;    // average tramsmit rate in bytes/s
+	double X_recv;   // rate seen by receiver
+	double t_RTO;    // tcp retransmission timeout value in secs, default = 4*R
+	double tld;     //time since last doubled during slow-start
+	double t_revcdata; // timestamp contained in ACK
+	double t_delay;    // t_delay contained in ACK
+	double t_now;
+	double R;       // Round trip time in seconds
+	float p;       // loss event rate
+	uint16_t b;     // max number of packets acknowledged by a single TCP ACK, default=1
+	
+	int alarmtimeout;
 
+
+	uint32_t cntrlTimeout; // timeout value for control message, default = 10 sec
+	uint32_t cntrlTimeoutCounter; //count at most 10 times for control msg
+	double noFeedbackTimer;
+	int feedbackRecvd;
 };
 
 
@@ -38,3 +65,6 @@ struct data_t data;
 struct ACK_t ack;
 struct ClientPrms tfrc_client;
 ClientStatus cStatus;
+int CNTCStop;
+
+void initializeparams();
